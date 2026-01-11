7b9914b4766517129a271a8eeaa6ed286d062e4e
unknown <tanabeyuto.tech@gmail.com>
2026-01-11 15:38:58 -0800

    feat: add space

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,7 @@
 /** @type import('hardhat/config').HardhatUserConfig */
 require("@nomicfoundation/hardhat-toolbox");
 
+// add space
 // require("@nomiclabs/hardhat-ethers");
 // require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
@@ -10,10 +11,10 @@ module.exports = {
   networks: {
   localhost: {
     url: "http://127.0.0.1:8545"
-  },
+  },   
   sepolia: {
     url: process.env.SEPOLIA_RPC_URL,
-    accounts: [process.env.PRIVATE_KEY]
+    accounts: [process.env.PRIVATE_KEY]   
   },
   mainnet: {
     url: process.env.MAINNET_RPC_URL,
