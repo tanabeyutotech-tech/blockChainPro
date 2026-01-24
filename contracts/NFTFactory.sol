5870a1b8d3aae95d78bc6d9558842dbe86f6d071
unknown <tanabeyuto.tech@gmail.com>
2026-01-24 11:59:19 +0900

    feat: NFTFactory.sol created

--- contracts/NFTFactory.sol
+++ contracts/NFTFactory.sol
@@ -1,48 +1,48 @@
-// // SPDX-License-Identifier: MIT
-// pragma solidity ^0.8.20;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
-// import "./Token.sol";
+import "./Token.sol";
 
-// contract NFTFactory {
-//     address[] public collections;
+contract NFTFactory {
+    address[] public collections;
 
-//     event CollectionCreated(
-//         address indexed creator,
-//         address indexed collection,
-//         string name,
-//         string symbol,
-//         string cover
-//     );
+    event CollectionCreated(
+        address indexed creator,
+        address indexed collection,
+        string name,
+        string symbol,
+        string cover
+    );
 
-//     function createCollection(
-//         string memory name,
-//         string memory symbol,
-//         string memory cover
-//     ) external {
-//         // deploy new ERC721 collection
-//         NFT newCollection = new NFT(
-//             name,
-//             symbol,
-//             cover,
-//             msg.sender
-//         );
+    function createCollection(
+        string memory name,
+        string memory symbol,
+        string memory cover
+    ) external {
+        // deploy new ERC721 collection
+        NFT newCollection = new NFT(
+            name,
+            symbol,
+            cover,
+            msg.sender
+        );
 
-//         collections.push(address(newCollection));
+        collections.push(address(newCollection));
 
-//         emit CollectionCreated(
-//             msg.sender,
-//             address(newCollection),
-//             name,
-//             symbol,
-//             cover
-//         );
-//     }
+        emit CollectionCreated(
+            msg.sender,
+            address(newCollection),
+            name,
+            symbol,
+            cover
+        );
+    }
 
-//     function getCollections() external view returns (address[] memory) {
-//         return collections;
-//     }
+    function getCollections() external view returns (address[] memory) {
+        return collections;
+    }
 
-//     function collectionsCount() external view returns (uint256) {
-//         return collections.length;
-//     }
-// }
+    function collectionsCount() external view returns (uint256) {
+        return collections.length;
+    }
+}
